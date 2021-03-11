af0a0c74726f12e91a26fcd977467f1d3462d7f0
Jorge Acosta Lemus <jorge@makingdevs.com>
2021-03-10 19:10:09 -0600

    script de groovy como portable

---
+++ script_file.groovy
@@ -0,0 +1,25 @@
+#!/usr/bin/env groovy
+
+println "Hola inmundo"
+
+// denoto un comentario de una linea
+
+println "otra linea "
+
+/*
+un comntario
+de multiples
+lineas
+*/
+
+println "mas codigo"
+
+/**
+* comentario para
+*/
+
+
+println new Date().class
+
+
+println "Makingdevs".toUpperCase()
