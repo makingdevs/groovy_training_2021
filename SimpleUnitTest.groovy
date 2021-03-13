029c2d6b3ddc0f6c1cec3104a141cc11d13d920c
Jose Juan Reyes <neodevelop@gmail.com>
2021-03-12 18:35:08 -0600

    Agrego una prueba y genericos

---
+++ SimpleUnitTest.groovy
@@ -0,0 +1,8 @@
+import static org.junit.Assert.assertEquals
+import org.junit.Test
+class SimpleUnitTest {
+  @Test
+  void shouldAdd() {
+    assertEquals("Groovy should add correctly", 2, 1 + 1)
+  }
+}
