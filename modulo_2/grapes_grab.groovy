ade7bd9b4beea3633942ebae8320b7a75cbfc856
Jose Juan Reyes <neodevelop@gmail.com>
2021-03-24 18:57:43 -0600

    Using grabs and grape

---
+++ modulo_2/grapes_grab.groovy
@@ -0,0 +1,22 @@
+@Grapes([
+  @Grab(group='org.apache.camel', module='camel-core', version='3.8.0'),
+  @Grab(group='org.apache.camel', module='camel-file', version='3.8.0')
+  //@Grab('org.apache.camel:camel-core:3.8.0')
+])
+
+import org.apache.camel.impl.DefaultCamelContext
+import org.apache.camel.builder.RouteBuilder
+
+def camelContext = new DefaultCamelContext()
+
+camelContext.addRoutes(new RouteBuilder() {
+  def void configure(){
+    from("file://inputdir/?delete=true").to("file://outputdir")
+  }
+})
+
+camelContext.start()
+println "Camel started"
+
+addShutdownHook { camelContext.stop() }
+synchronized(this) { this.wait() }
