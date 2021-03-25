01e07951b25a6b7280f0c91e7a19f6b5cc0695a5
Jose Juan Reyes <neodevelop@gmail.com>
2021-03-24 19:10:07 -0600

    Usando biblioteca de terceros

--- modulo_2/grapes_grab.groovy
+++ modulo_2/grapes_grab.groovy
@@ -1,6 +1,7 @@
 @Grapes([
   @Grab(group='org.apache.camel', module='camel-core', version='3.8.0'),
   @Grab(group='org.apache.camel', module='camel-file', version='3.8.0')
+  //@Grab(group='com.proveedor', module='super.biblioteca', version='2.33')
   //@Grab('org.apache.camel:camel-core:3.8.0')
 ])
 
