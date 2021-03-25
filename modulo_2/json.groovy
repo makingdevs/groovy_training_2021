7a5bf894dc099ab92bc77194ee90c46e45bcbb88
Jose Juan Reyes <neodevelop@gmail.com>
2021-03-24 18:30:00 -0600

    Parseo de JSON

---
+++ modulo_2/json.groovy
@@ -0,0 +1,22 @@
+import groovy.json.*
+
+payload = new URL("https://api.github.com/repos/makingdevs/MyBarista/issues").text
+
+jsonSlurper = new JsonSlurper()
+
+document = jsonSlurper.parseText(payload)
+
+document.each { issue ->
+  println "*"*100
+  println """
+  ID: ${issue.id}
+  TITLE: ${issue.title.trim()}
+  BODY: ${issue.body.trim()}
+  LABELS:
+  """
+  issue.labels.each { label ->
+    println """
+    - ${label.name.trim()}
+    """
+  }
+}
