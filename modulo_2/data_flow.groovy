f83fd7264b93bd2e90ae5093c3f187cb2093205c
Jose Juan Reyes <neodevelop@gmail.com>
2021-03-26 20:11:58 -0600

    Data Flow con Groovy - CSP

---
+++ modulo_2/data_flow.groovy
@@ -0,0 +1,9 @@
+import groovyx.gpars.dataflow.Dataflows
+import static groovyx.gpars.dataflow.Dataflow.task
+
+def flow = new Dataflows()
+task { flow.result = flow.x + flow.y }
+task { flow.x = 10 }
+task { flow.y = 5 }
+assert 15 == flow.result
+println flow.result
