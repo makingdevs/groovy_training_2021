efd5f98b34c7589604eb4dfdc580814b55aee3c0
Jose Juan Reyes <neodevelop@gmail.com>
2021-03-26 18:45:27 -0600

    Usando @Canonical en conjunto

--- modulo_2/generacion_de_codigo.groovy
+++ modulo_2/generacion_de_codigo.groovy
@@ -1,7 +1,7 @@
+@groovy.transform.Canonical
 //@groovy.transform.TupleConstructor
 //@groovy.transform.ToString(includeNames=true)
-//@groovy.transform.EqualsAndHashCode
-@groovy.transform.Canonical
+@groovy.transform.EqualsAndHashCode(includes = ["nombre", "duracion"])
 class Curso {
 
   String nombre
@@ -11,7 +11,7 @@ class Curso {
 }
 
 curso1 = new Curso("Groovy intermedio", 20, 12364.21)
-curso2 = new Curso("Groovy intermedio", 20, 12364.21)
+curso2 = new Curso("Groovy intermedio", 20, 43432.21)
 println curso1
 println curso2
 println curso1 == curso2
